98426bb258df244242a9dc6f72de74b4df475680
EduardoBerlitz <duduberlitz@hotmail.com>
2026-08-28 19:02:36 -0300

    API Trabalho Final M7

---
+++ src/server.js
@@ -0,0 +1,100 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+
+import {
+    conectarBanco,
+    livrosCollection
+} from "./db.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/health", (_req, res) => {
+    res.json({ status: "ok" });
+});
+
+app.get("/api/livros", async (_req, res) => {
+    try {
+        const livros =
+            await livrosCollection()
+                .find(
+                    {},
+                    { projection: { _id: 0 } }
+                )
+                .sort({ id: 1 })
+                .toArray();
+
+        res.json(livros);
+
+    } catch {
+        res.status(500).json({
+            erro: "Erro ao listar livros."
+        });
+    }
+});
+
+app.get("/api/livros/:id", async (req, res) => {
+    const id = Number(req.params.id);
+
+    if (!Number.isFinite(id)) {
+        return res.status(400).json({
+            erro: "ID inválido."
+        });
+    }
+
+    const livro =
+        await livrosCollection().findOne(
+            { id },
+            { projection: { _id: 0 } }
+        );
+
+    if (!livro) {
+        return res.status(404).json({
+            erro: "Livro não encontrado."
+        });
+    }
+
+    res.json(livro);
+});
+
+app.post("/api/livros", async (req, res) => {
+    const {
+        titulo,
+        autor,
+        categoria,
+        ano,
+        status,
+        descricao,
+    } = req.body;
+
+    if (!titulo || !autor || !ano || !status || !descricao) {
+        return res.status(400).json({
+            erro: "Dados obrigatórios ausentes."
+        });
+    }
+
+    const novoLivro = {
+        id: Date.now(),
+        titulo,
+        autor,
+        categoria,
+        ano,
+        status,
+        descricao
+    };
+
+    await livrosCollection().insertOne(novoLivro);
+    res.status(201).json(novoLivro);
+});
+
+const PORT =
+    Number(process.env.PORT) || 3000;
+
+await conectarBanco();
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`API em http://localhost:${PORT}`);
+});
